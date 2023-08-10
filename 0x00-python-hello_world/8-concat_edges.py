9b1d97379ee54de1b145610a873ca9bf6ec5d9e0
Joshua Wilfred <joshuakaaya216@gmail.com>
2023-08-09 19:37:39 -0700

    Upload

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+
+ new_str = str[39:66] + " " + str[103:106] + " " + str[0:6]
+ print(new_str)
