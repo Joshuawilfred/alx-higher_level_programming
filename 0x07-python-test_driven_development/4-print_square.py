8f796c83b3a5054da569d8d3db60788d6914701e
Joshua Wilfred <joshuakaaya216@gmail.com>
2023-09-05 20:03:19 -0700

    Indexing Module

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Square Module"""
+
+
+def print_square(size):
+    """ Prints a square based on the argument size
+    Args:
+        size (int): Size of the square to be printed
+    """
+
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if size > 0:
+        for row in range(size):
+            print("#"*size)
