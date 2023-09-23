180f93901aa055a4832b18cf3fc40301260b9084
Joshua Wilfred <joshuakaaya216@gmail.com>
2023-09-22 19:48:16 -0700

    Initial test run

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangles.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangles.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Defines unit tests for models/rectangle.py.
+# test_rectangle.py
+"""Defines unittests for models/rectangle.py.
+
 Unittest classes:
     TestRectangle_instantiation - line 25
     TestRectangle_width - line 114
@@ -120,7 +122,6 @@ class TestRectangle_width(unittest.TestCase):
             Rectangle("invalid", 2)
 
     def test_float_width(self):
-??? from here until ???END lines may have been inserted/deleted
         with self.assertRaisesRegex(TypeError, "width must be an integer"):
             Rectangle(5.5, 1)
 
@@ -189,9 +190,10 @@ class TestRectangle_height(unittest.TestCase):
     """Unittests for testing initialization of Rectangle height attribute."""
 
     def test_None_height(self):
-        with self.asse
+        with self.assertRaisesRegex(TypeError, "height must be an integer"):
+            Rectangle(1, None)
 
- def test_str_height(self):
+    def test_str_height(self):
         with self.assertRaisesRegex(TypeError, "height must be an integer"):
             Rectangle(1, "invalid")
 
@@ -453,6 +455,7 @@ class TestRectangle_stdout(unittest.TestCase):
     @staticmethod
     def capture_stdout(rect, method):
         """Captures and returns text printed to stdout.
+
         Args:
             rect (Rectangle): The Rectangle to print to stdout.
             method (str): The method to run on rect.
